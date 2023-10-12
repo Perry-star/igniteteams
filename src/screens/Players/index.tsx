b67b6501fa98e110ac43393605758e7430f65031
Perry-star <mateus.ramosilva@gmail.com>
2023-10-11 22:01:14 -0300

    new Version

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,18 +1,22 @@
-import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+
+import { FlatList } from "react-native";
+import { useState } from "react";
+
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { FlatList } from "react-native";
-import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
-export function Players(){
+export function Players() {
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState(['Matheus']);
+    const [players, setPlayers] = useState(['Matheus', 'Marcos', 'Thalia', 'Margarete', 'Gilvan', 'Beatriz', 'Thiago', 'Jefferson']);
 
-    return(
+    return (
         <Container>
             <Header
                 showBackButton
@@ -27,47 +31,60 @@ export function Players(){
 
                 <Input
                     placeholder="  Nome da pessoa"
-                    autoCorrect={false}                    
+                    autoCorrect={false}
                 />
-                            
+
                 <ButtonIcon
                     icon="add"
                 />
             </Form>
 
-        <HeaderList>
-            <FlatList
-               data={["Time A ", "Time B"]}
-               keyExtractor={item => item}
-               renderItem={({ item }) => (
-                   <Filter
-                       title={item}
-                       isActive={item === team}
-                       onPress={() => setTeam(item)}
+            <HeaderList>
+                <FlatList
+                    data={["Time A ", "Time B"]}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
 
-               />
-           )}
-           horizontal
-           />            
-
-           <NumbersOfPlayers>
-                {players.length}
-            </NumbersOfPlayers> 
-        </HeaderList>
-
-        <FlatList
-            data={players}
-            keyExtractor={item => item}
-            renderItem={({ item }) => (
-                <PlayerCard
-                   name={item}
-                   onRemove={() => { }}
+                        />
+                    )}
+                    horizontal
                 />
-            )}
-        />
-        
 
-            
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+            </HeaderList>
+
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <PlayerCard
+                        name={item}
+                        onRemove={() => { }}
+                    />
+                )}
+
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message="Nenhum membro nesse time."
+                    />
+                    
+                )}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={[{paddingBottom: 100}, players.length === 0 && { flex:1}]}
+            />
+
+            <Button
+                title="Remover Turma"
+                type="SECUNDARY"
+            />
+
+
         </Container>
     )
 }
